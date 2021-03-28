7da5e10fcd0c87ad1e9154b62b5dd3e78cdfeea1
KirillZorikov <k.zorikov@yandex.ru>
2021-03-28 12:04:24 +0500

    update store for auth

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,11 @@
 import { createStore } from 'vuex'
-
+import { auth } from './auth.module';
 
 
 export const store = createStore({
+  modules: {
+    auth
+  },
   state: {
     backendUrl: "http://127.0.0.1:8000/api/v1"
   },
