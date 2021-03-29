d95db412f3cc7520d6cffe7a196fb682b4f688cf
KirillZorikov <k.zorikov@yandex.ru>
2021-03-29 13:40:28 +0500

    add handle unauthorized access

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,33 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from '../views/Login.vue';
-import Logout from '../views/Logout.vue';
+import Home from '@/views/Home.vue'
+import Login from '@/views/auth/Login.vue';
+import Logout from '@/views/auth/Logout.vue';
 import Post from "@/views/Post";
-import Register from "@/views/Register";
-import ChangePassword from "@/views/ChangePassword";
+import Register from "@/views/auth/Register";
+import ChangePassword from "@/views/auth/ChangePassword";
 
 const routes = [
     {
         path: '/',
         name: 'Home',
-        component: Home
+        component: Home,
+        props: true
     },
     {
-      path: '/login',
-      component: Login
+        path: '/login',
+        component: Login
     },
     {
-      path: '/logout',
-      component: Logout
+        path: '/logout',
+        component: Logout
     },
     {
-      path: '/change-password',
-      component: ChangePassword
+        path: '/change-password',
+        component: ChangePassword
     },
     {
-      path: '/register',
-      component: Register
+        path: '/register',
+        component: Register
     },
     {
         path: '/post/:id',
@@ -48,5 +49,19 @@ const router = createRouter({
     history: createWebHashHistory(),
     routes
 })
+
+router.beforeEach((to, from, next) => {
+    const publicPages = ['/login', '/register', '/', '/post/:id'];
+    const authRequired = !publicPages.includes(to.path);
+    const loggedIn = localStorage.getItem('user');
+
+    // trying to access a restricted page + not logged in
+    // redirect to login page
+    if (authRequired && !loggedIn) {
+        next('/login');
+    } else {
+        next();
+    }
+});
 
 export default router
