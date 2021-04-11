970718047411c44e8542af7e7946154500ed00af
kirill <k.zorikov@yandex.ru>
2021-04-11 13:05:40 +0500

    refactoring

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -5,30 +5,37 @@ const API_URL = 'http://127.0.0.1:8000/api/v1/';
 
 class UserService {
     async getListPosts(ordering, page, filtering) {
-        let params = ordering ? {ordering: ordering.toString()}: {};
-        params = Object.assign({}, params, filtering ? filtering: {});
-        params = Object.assign({}, params, page ? {page: page}: {});
-        console.log(params)
+        if (!ordering.includes('pub_date')) {
+            ordering = [ordering, '-pub_date'];
+        }
+        let params = ordering ? {ordering: ordering.toString()} : {};
+        params = Object.assign({}, params, filtering ? filtering : {});
+        params = Object.assign({}, params, page ? {page: page} : {});
         return await axios.get(API_URL + `posts`, {
             headers: authHeader(),
             params: params,
         });
     }
+
     async getPost(id) {
         return await axios.get(API_URL + `posts/${id}`, {headers: authHeader()});
     }
-    async getListGroups(ordering=['-posts_count', 'title']) {
+
+    async getListGroups(ordering = ['-posts_count', 'title']) {
         return await axios.get(API_URL + 'groups', {
             headers: authHeader(),
             params: {ordering: ordering.toString()},
         });
     }
+
     async getGroup(slug) {
         return await axios.get(API_URL + `groups/${slug}`, {headers: authHeader()});
     }
+
     async getListTags() {
         return await axios.get(API_URL + 'tags', {headers: authHeader()});
     }
+
     async getAuthorInfo(username) {
         return await axios.get(API_URL + `users/${username}`, {headers: authHeader()});
     }
