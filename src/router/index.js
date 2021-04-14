3efad255aafb2a28ceb8adaa832fe5cb6e732cd9
kirill <k.zorikov@yandex.ru>
2021-04-14 12:49:37 +0500

    add smooth scroll

--- src/router/index.js
+++ src/router/index.js
@@ -69,7 +69,13 @@ const routes = [
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
-    routes
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        return window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
 })
 
 router.beforeEach((to, from, next) => {
